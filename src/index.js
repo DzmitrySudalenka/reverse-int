e33afff1ee2bd6be8ee1487b57852f8a11223ba2
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-07 12:32:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
   
+  const absNum = Math.abs(n);
+  
+  const reversedNumStr = String(absNum).split('').reverse().join('');
+  
+  const resultNum = Number(reversedNumStr);
+  
+  return resultNum;
+  
 }
